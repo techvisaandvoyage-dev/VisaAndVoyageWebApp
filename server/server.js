9d36e5e74c379beffd384299d75bd5e246f55ada
VisaAndVoyage <tech.visaandvoyage@gmail.com>
2026-06-13 17:47:10 +0530

    fix: add localhost:5175-5177 ports to CORS allowedOrigins

--- server/server.js
+++ server/server.js
@@ -24,9 +24,15 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
   "http://localhost:5174",
+  "http://localhost:5175",
+  "http://localhost:5176",
+  "http://localhost:5177",
   "http://127.0.0.1:3000",
   "http://127.0.0.1:5173",
-  "http://127.0.0.1:5174"
+  "http://127.0.0.1:5174",
+  "http://127.0.0.1:5175",
+  "http://127.0.0.1:5176",
+  "http://127.0.0.1:5177"
 ];
 
 // Middleware
