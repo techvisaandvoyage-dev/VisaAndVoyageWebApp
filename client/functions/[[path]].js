a2ac9c0664a888173703906e60e107c04dbe4f43
VisaAndVoyage <tech.visaandvoyage@gmail.com>
2026-06-23 21:32:30 +0530

    Fix syntax error in Cloudflare functions

--- client/functions/[[path]].js
+++ client/functions/[[path]].js
@@ -38,7 +38,7 @@ export async function onRequest(context) {
       const ogDescription = config.seoOpenGraphDescription || description;
       const twitterTitle = config.seoTwitterTitle || title;
       const twitterDescription = config.seoTwitterDescription || description;
-      const robots = config.seoRobotsIndex  "index,follow" : "noindex,nofollow";
+      const robots = config.seoRobotsIndex ? "index,follow" : "noindex,nofollow";
       
       const faviconUrl = config.seoFaviconUrl || "";
       const favicon32 = config.seoFavicon32Url || faviconUrl;
